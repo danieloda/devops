c5602453e694f60e0edbd85bc6094b96f97df0e1
Daniel Ashton Oda <danieloda@MacBook-Air-de-Daniel.local>
2026-02-27 21:14:27 -0300

    feat: add logo and restaurant name to login page (#12)
    Add Restaurante Planalto branding with logo image and styled login form
    
    closes #12

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function connectWithRetry() {
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
